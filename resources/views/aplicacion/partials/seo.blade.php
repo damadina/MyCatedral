b95cb92362e5131993b453acca27f59dd6fb03a8
damadina <carlos.marti.mallen@gmail.com>
2024-01-02 19:44:12 +0100

    llllll

--- resources/views/aplicacion/partials/seo.blade.php
+++ resources/views/aplicacion/partials/seo.blade.php
@@ -1,4 +1,4 @@
 <title>{{$elemento->seoTitle}}</title>
 <meta name="description" content="{{Str::limit($elemento->seoMeta,150)}}">
 <link rel="icon" href="{{ asset('storage/images/logo.png') }}">
-<link rel="canonical" href="{{url()->current();}}" />
+<link rel="canonical" href="{{url()->current().'/'}}" />
