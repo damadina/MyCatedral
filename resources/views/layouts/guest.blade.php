5f1f11848d6725ebe19db8b6a2d79f28a2d0b0bf
damadina <carlos.marti.mallen@gmail.com>
2024-01-03 11:43:53 +0100

    wwwwwwwwwwwwwwwwww

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="robots" content="noindex">
         <link rel="icon" href="{{asset("storage/images/logo.png")}}">
         <title>{{ config('app.name', 'Catedral de Santiago') }}</title>
 
