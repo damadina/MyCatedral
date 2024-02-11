6afd1ba0445a83e693ca9bc38375ce0ab0a9ba3b
damadina <carlos.marti.mallen@gmail.com>
2024-02-11 19:52:24 +0100

    aaaaaaaaaaaaaaaa

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -9,9 +9,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-
+                    <a href='\'>
                         <x-application-mark class="block h-9 w-auto" />
-
+                    </a>
                 </div>
 
 
